414e2549048c171127474018c080e070017b6cd9
Luke100000 <jonas.peche@aon.at>
2019-03-31 11:38:34 +0200

    moved matrix library inside the engine

--- 3DreamEngine/init.lua
+++ 3DreamEngine/init.lua
@@ -63,6 +63,7 @@ _3DreamEngine = lib
 require((...) .. "/functions")
 require((...) .. "/shader")
 require((...) .. "/loader")
+matrix = require((...) .. "/matrix")
 _3DreamEngine = nil
 
 lib.cam = {x = 0, y = 0, z = 0, rx = 0, ry = 0, rz = 0, normal = {0, 0, 0}}
