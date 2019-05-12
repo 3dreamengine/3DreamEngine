52f8a1c99cd129ebe2379ff81a340c1d5a2b621a
Luke100000 <jonas.peche@aon.at>
2019-05-12 14:14:34 +0200

    added example selected

--- main.lua
+++ main.lua
@@ -1,1 +1,15 @@
-require("examples/knight/main")
+local examples = love.filesystem.getDirectoryItems("examples")
+
+function love.draw()
+	love.graphics.print("press number to start example", 50, 50)
+	
+	for d,s in ipairs(examples) do
+		love.graphics.print(d .. ": " .. s, 50, 50 + d*20)
+	end
+end
+
+function love.keypressed(key)
+	if tonumber(key) and examples[tonumber(key)] then
+		require("examples/" .. examples[tonumber(key)] .. "/main")
+	end
+end
