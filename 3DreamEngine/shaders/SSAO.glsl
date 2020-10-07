6ae7576977db000ba78e36f5719a9eaec2b4ddf1
Luke100000 <jonas.peche@aon.at>
2020-10-07 11:56:48 +0200

    minor SSAO improvement

--- 3DreamEngine/shaders/SSAO.glsl
+++ 3DreamEngine/shaders/SSAO.glsl
@@ -1,27 +1,16 @@
 extern mediump vec3 samples[SAMPLE_COUNT];
 
-const float maxDistanceInverse = 10.0;
-
 vec4 effect(vec4 color, Image texture, vec2 tc, vec2 sc) {
 	float sum = 0.0;
 	
 	float depth = Texel(texture, tc).r;
-	if (depth >= 250.0) {
-		return vec4(1.0);
-	} else {
-		for (int i = 0; i < SAMPLE_COUNT; i++) {
-			float sampleDepth = Texel(texture, tc + samples[i].xy / (0.25+depth)).r;
-			
-			//samples differences (but clamps it)
-			if (sampleDepth < 250.0) {
-				float diff = depth - sampleDepth;
-				float f = clamp(5.0 - abs(diff) * maxDistanceInverse * (0.25 + depth * 0.25), 0.0, 1.0);
-				sum += clamp(diff * 8.0, -1.0, 1.0) * samples[i].z * f;
-			}
-		}
+	for (int i = 0; i < SAMPLE_COUNT; i++) {
+		float sampleDepth = Texel(texture, tc + samples[i].xy / depth).r;
 		
-		//strength
-		sum = min(1.0, 1.0 - max(0.0, sum) * 4.0);
-		return vec4(sum, sum, sum, 1.0);
+		float diff = depth - sampleDepth;
+		sum += clamp(diff, -1.0, 1.0) * samples[i].z / (abs(diff)+0.05);
 	}
+	
+	sum = clamp(1.0 - sum * 2.0, 0.0, 1.0);
+	return vec4(sum, sum, sum, 1.0);
 }
