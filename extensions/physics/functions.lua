52d1214ad07b327c96fda8eec74c35eb07ad1cd5
Luke100000 <jonas.peche@aon.at>
2022-01-17 17:51:48 +0100

    simplified mesh colliders

--- extensions/physics/functions.lua
+++ extensions/physics/functions.lua
@@ -60,8 +60,14 @@ function lib:getMeshPhysicsObject(phy, transform)
 	end
 	
 	--convert normals
+	local subm = transform:subm()
+	local transformed = { }
+	local normals = { }
 	for d,s in ipairs(phy.normals) do
-		phy.normals[d] = vec3(s)
+		if not transformed[s] then
+			transformed[s] = subm and subm * vec3(s) or vec3(s)
+		end
+		normals[d] = transformed[s]
 	end
 	
 	local normals = phy.normals
@@ -70,174 +76,172 @@ function lib:getMeshPhysicsObject(phy, transform)
 	local bothSides = physicsLowerMode == "complex" or physicsLowerMode == "full"
 	local normalThreshold = 0.01
 	
-	for group, faces in ipairs(phy.groups) do
-		--create vertex group for faster access
-		lib.deltonLoad:start("height")
-		local g = { }
-		local verts_top = { }
-		local verts_bottom = { }
-		local sides = { }
-		local lowest = math.huge
-		for faceID,face in ipairs(faces) do
-			--store all vertices in a threshold sized 2D grid for fast lookups
+	--create vertex group for faster access
+	lib.deltonLoad:start("height")
+	local g = { }
+	local verts_top = { }
+	local verts_bottom = { }
+	local sides = { }
+	local lowest = math.huge
+	
+	for faceID, face in ipairs(phy.faces) do
+		--store all vertices in a threshold sized 2D grid for fast lookups
+		local a = vertices[face[1]]
+		local b = vertices[face[2]]
+		local c = vertices[face[3]]
+		insert(a, g)
+		insert(b, g)
+		insert(c, g)
+		
+		--wether it is a top faced triangle or not
+		local n = (b-a):cross(c-a):normalize().y
+		
+		--remember lowest for simple lower mode
+		lowest = math.min(lowest, a.y, b.y, c.y)
+		
+		--add either to the top or bottom list, instead of remembering the side for each vertex
+		if n > normalThreshold then
+			verts_top[face[1]] = a
+			verts_top[face[2]] = b
+			verts_top[face[3]] = c
+			sides[faceID] = true
+		elseif bothSides and n < -normalThreshold then
+			verts_bottom[face[1]] = a
+			verts_bottom[face[2]] = b
+			verts_bottom[face[3]] = c
+			sides[faceID] = false
+		end
+	end
+	
+	local opposite = { }
+	for side = 1, 2 do
+		for d,s in pairs(side == 1 and verts_top or verts_bottom) do
+			if physicsLowerMode == "height" then
+				opposite[d] = -math.huge
+			elseif physicsLowerMode == "simple" then
+				opposite[d] = lowest
+			end
+			
+			--find the bottom most vertex
+			if not opposite[d] then
+				lib.deltonLoad:start("find")
+				for x = math.floor(s.x * threshold - 0.5), math.floor(s.x * threshold + 0.5) do
+					if g[x] then
+						for z = math.floor(s.z * threshold - 0.5), math.floor(s.z * threshold + 0.5) do
+							if g[x][z] then
+								if side == 1 then
+									opposite[d] = math.min(opposite[d] or s.y, g[x][z][1])
+								else
+									opposite[d] = math.max(opposite[d] or s.y, g[x][z][2])
+								end
+							end
+						end
+					end
+				end
+				lib.deltonLoad:stop()
+			end
+			
+			--no real opposite vertex found, interpolate and retriangulate opposite face
+			if math.abs(opposite[d] - s.y) == 0 then
+				if physicsLowerMode == "complex" then
+					lib.deltonLoad:start("interpolate")
+					for faceID, face in ipairs(phy.faces) do
+						--vertices
+						local a = vertices[face[1]]
+						local b = vertices[face[2]]
+						local c = vertices[face[3]]
+						
+						local w1, w2, w3 = self:getBarycentric(s.x, s.z, a.x, a.z, b.x, b.z, c.x, c.z)
+						local inside = w1 >= 0 and w2 >= 0 and w3 >= 0 and w1 <= 1 and w2 <= 1 and w3 <= 1
+						if inside then
+							local h = a.y * w1 + b.y * w2 + c.y * w3
+							
+							if side == 1 and h < s.y or side == 2 and h > s.y then
+								opposite[d] = h
+								goto done
+							end
+						end
+					end
+					::done::
+					lib.deltonLoad:stop()
+				else
+					opposite[d] = lowest
+				end
+			end
+		end
+	end
+	lib.deltonLoad:stop()
+	
+	--create polygons
+	lib.deltonLoad:start("triangles")
+	local cache = { }
+	for faceID,face in ipairs(phy.faces) do
+		--verify
+		local side = sides[faceID]
+		if side ~= nil then
+			--vertices
 			local a = vertices[face[1]]
 			local b = vertices[face[2]]
 			local c = vertices[face[3]]
-			insert(a, g)
-			insert(b, g)
-			insert(c, g)
 			
-			--wether it is a top faced triangle or not
-			local n = (b-a):cross(c-a):normalize().y
+			--create physics shape
+			local ok, shape = pcall(love.physics.newPolygonShape, a.x, a.z, b.x, b.z, c.x, c.z)
 			
-			--remember lowest for simple lower mode
-			lowest = math.min(lowest, a.y, b.y, c.y)
-			
-			--add either to the top or bottom list, instead of remembering the side for each vertex
-			if n > normalThreshold then
-				verts_top[face[1]] = a
-				verts_top[face[2]] = b
-				verts_top[face[3]] = c
-				sides[faceID] = true
-			elseif bothSides and n < -normalThreshold then
-				verts_bottom[face[1]] = a
-				verts_bottom[face[2]] = b
-				verts_bottom[face[3]] = c
-				sides[faceID] = false
-			end
-		end
-		
-		local opposite = { }
-		for side = 1, 2 do
-			for d,s in pairs(side == 1 and verts_top or verts_bottom) do
-				if physicsLowerMode == "height" then
-					opposite[d] = -math.huge
-				elseif physicsLowerMode == "simple" then
-					opposite[d] = lowest
-				end
+			if ok then
+				--reconstruct the order, since the polygon might have restructured itself
+				local x1, y1, x2, y2, x3, y3 = shape:getPoints()
+				local translation = {
+					[smallest(a.x, a.z, x1, y1, x2, y2, x3, y3)] = face[1],
+					[smallest(b.x, b.z, x1, y1, x2, y2, x3, y3)] = face[2],
+					[smallest(c.x, c.z, x1, y1, x2, y2, x3, y3)] = face[3],
+				}
 				
-				--find the bottom most vertex
-				if not opposite[d] then
-					lib.deltonLoad:start("find")
-					for x = math.floor(s.x * threshold - 0.5), math.floor(s.x * threshold + 0.5) do
-						if g[x] then
-							for z = math.floor(s.z * threshold - 0.5), math.floor(s.z * threshold + 0.5) do
-								if g[x][z] then
-									if side == 1 then
-										opposite[d] = math.min(opposite[d] or s.y, g[x][z][1])
-									else
-										opposite[d] = math.max(opposite[d] or s.y, g[x][z][2])
-									end
-								end
+				--avoid duplicates
+				local ID = math.floor(math.min(a.x, b.x, c.x) * 1000)
+				local center = a + b + c
+				for i = ID-1, ID+1 do
+					if cache[i] then
+						for _,c in pairs(cache[i]) do
+							if (center.x - c.x)^2 + (center.z - c.z)^2 < 1 / 1000 then
+								ID = nil
+								break
 							end
 						end
 					end
-					lib.deltonLoad:stop()
 				end
 				
-				--no real opposite vertex found, interpolate and retriangulate opposite face
-				if math.abs(opposite[d] - s.y) == 0 then
-					if physicsLowerMode == "complex" then
-						lib.deltonLoad:start("interpolate")
-						local g = phy.groups[group]
-						for faceID, face in ipairs(g) do
-							--vertices
-							local a = vertices[face[1]]
-							local b = vertices[face[2]]
-							local c = vertices[face[3]]
-							
-							local w1, w2, w3 = self:getBarycentric(s.x, s.z, a.x, a.z, b.x, b.z, c.x, c.z)
-							local inside = w1 >= 0 and w2 >= 0 and w3 >= 0 and w1 <= 1 and w2 <= 1 and w3 <= 1
-							if inside then
-								local h = a.y * w1 + b.y * w2 + c.y * w3
-								
-								if side == 1 and h < s.y or side == 2 and h > s.y then
-									opposite[d] = h
-									goto done
-								end
-							end
-						end
-						::done::
-						lib.deltonLoad:stop()
-					else
-						opposite[d] = lowest
-					end
-				end
-			end
-		end
-		lib.deltonLoad:stop()
-		
-		--create polygons
-		lib.deltonLoad:start("triangles")
-		local cache = { }
-		for faceID,face in ipairs(faces) do
-			--verify
-			local side = sides[faceID]
-			if side ~= nil then
-				--vertices
-				local a = vertices[face[1]]
-				local b = vertices[face[2]]
-				local c = vertices[face[3]]
-				
-				--create physics shape
-				local ok, shape = pcall(love.physics.newPolygonShape, a.x, a.z, b.x, b.z, c.x, c.z)
-				
-				if ok then
-					--reconstruct the order, since the polygon might have restructured itself
-					local x1, y1, x2, y2, x3, y3 = shape:getPoints()
-					local translation = {
-						[smallest(a.x, a.z, x1, y1, x2, y2, x3, y3)] = face[1],
-						[smallest(b.x, b.z, x1, y1, x2, y2, x3, y3)] = face[2],
-						[smallest(c.x, c.z, x1, y1, x2, y2, x3, y3)] = face[3],
-					}
+				if ID then
+					cache[ID] = cache[ID] or { }
+					table.insert(cache[ID], center)
 					
-					--avoid duplicates
-					local ID = math.floor(math.min(a.x, b.x, c.x) * 1000)
-					local center = a + b + c
-					for i = ID-1, ID+1 do
-						if cache[i] then
-							for _,c in pairs(cache[i]) do
-								if (center.x - c.x)^2 + (center.z - c.z)^2 < 1 / 1000 then
-									ID = nil
-									break
-								end
-							end
-						end
-					end
+					--add shape
+					table.insert(n.objects, shape)
 					
-					if ID then
-						cache[ID] = cache[ID] or { }
-						table.insert(cache[ID], center)
-						
-						--add shape
-						table.insert(n.objects, shape)
-						
-						--face normal
-						table.insert(n.normals, {
-							normals[translation[1]] * (side and 1 or -1),
-							normals[translation[2]] * (side and 1 or -1),
-							normals[translation[3]] * (side and 1 or -1),
-						})
-						
-						--triangle height
-						table.insert(n[side and "highest" or "lowest"], {
-							vertices[translation[1]].y,
-							vertices[translation[2]].y,
-							vertices[translation[3]].y,
-						})
-						
-						--triangle lowest
-						table.insert(n[side and "lowest" or "highest"], {
-							opposite[translation[1]],
-							opposite[translation[2]],
-							opposite[translation[3]],
-						})
-					end
+					--face normal
+					table.insert(n.normals, {
+						normals[translation[1]] * (side and 1 or -1),
+						normals[translation[2]] * (side and 1 or -1),
+						normals[translation[3]] * (side and 1 or -1),
+					})
+					
+					--triangle height
+					table.insert(n[side and "highest" or "lowest"], {
+						vertices[translation[1]].y,
+						vertices[translation[2]].y,
+						vertices[translation[3]].y,
+					})
+					
+					--triangle lowest
+					table.insert(n[side and "lowest" or "highest"], {
+						opposite[translation[1]],
+						opposite[translation[2]],
+						opposite[translation[3]],
+					})
 				end
 			end
 		end
-		lib.deltonLoad:stop()
 	end
+	lib.deltonLoad:stop()
 	
 	return n
 end
