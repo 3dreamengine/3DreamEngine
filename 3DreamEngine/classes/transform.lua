d83601378c08e83c989b2478c6661f291d4b4aa2
Luke100000 <jonas.peche@aon.at>
2023-02-05 00:06:29 +0100

    cleanup

--- 3DreamEngine/classes/transform.lua
+++ 3DreamEngine/classes/transform.lua
@@ -10,8 +10,7 @@ end
 
 function class:setTransform(t)
 	self.transform = t
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
@@ -21,72 +20,62 @@ end
 
 function class:translate(x, y, z)
 	self.transform = self.transform and self.transform:translate(x, y, z) or mat4.getTranslate(x, y, z)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:scale(x, y, z)
 	self.transform = self.transform and self.transform:scale(x, y, z) or mat4.getScale(x, y, z)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateX(rx)
 	self.transform = self.transform and self.transform:rotateX(rx) or mat4.getRotateX(rx)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateY(ry)
 	self.transform = self.transform and self.transform:rotateY(ry) or mat4.getRotateY(ry)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateZ(rz)
 	self.transform = self.transform and self.transform:rotateZ(rz) or mat4.getRotateZ(rz)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 --todo optimize world transforms
 function class:translateWorld(x, y, z)
 	self.transform = self.transform and (mat4.getTranslate(x, y, z) * self.transform) or mat4.getTranslate(x, y, z)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:scaleWorld(x, y, z)
 	self.transform = self.transform and (mat4.getScale(x, y, z) * self.transform) or mat4.getScale(x, y, z)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateXWorld(rx)
 	self.transform = self.transform and (mat4.getRotateX(rx) * self.transform) or mat4.getRotateX(rx)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateYWorld(ry)
 	self.transform = self.transform and (mat4.getRotateY(ry) * self.transform) or mat4.getRotateY(ry)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
 function class:rotateZWorld(rz)
 	self.transform = self.transform and (mat4.getRotateZ(rz) * self.transform) or mat4.getRotateZ(rz)
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
@@ -99,6 +88,11 @@ function class:lookAt(position, up)
 	position = self:getInvertedTransform() * position
 	up = self:getInvertedTransform():subm() * (up or vec3(0.0, 1.0, 0.0))
 	self:lookTowards(position, up)
+end
+
+function class:setDirty()
+	self.inverseTransform = false
+	self.dynamic = true
 end
 
 function class:lookTowards(direction, up)
@@ -118,8 +112,7 @@ function class:lookTowards(direction, up)
 	--todo someone with better math skills should take a look at this mess
 	self.transform = self:getTransform() * rotate:invert()
 	
-	self.inverseTransform = false
-	self.dynamic = true
+	self:setDirty()
 	return self
 end
 
